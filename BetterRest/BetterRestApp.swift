7e5ace7802a8bfea13c186cd970e3d381db6b650
archanab811 <archanabhosale7@gmail.com>
2023-04-30 09:58:53 +0530

    Sample project for SwiftUI

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Archana Mahesh Chigare on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
